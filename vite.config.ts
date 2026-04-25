02556d03b778959f822ee22508bf44e928b5b08e
Jaco Kok <kokjaco2@gmail.com>
2026-04-25 17:52:12 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { keycloakify } from "keycloakify/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    keycloakify({
+      accountThemeImplementation: "none",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
