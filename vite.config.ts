10945d2ed02cc8a19fbc6c394c56d13651458f0e
Jaco Kok <kokjaco2@gmail.com>
2026-05-14 14:27:28 +0200

    New theme updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -11,11 +11,30 @@ export default defineConfig({
     tailwindcss(),
     keycloakify({
       accountThemeImplementation: "none",
+      themeName: "your-theme-name",
+      environmentVariables: [
+        {
+          name: "SHADCN_THEME_LOGO_WHITE_URL",
+          default: "",
+        },
+        {
+          name: "SHADCN_THEME_LOGO_DARK_URL",
+          default: "",
+        },
+        { name: "SHADCN_THEME_APP_NAME", default: "Aviate Soft" },
+        { name: "SHADCN_THEME_LAYOUT", default: "centered-card" },
+        { name: "SHADCN_THEME_SIDE_IMAGE_URL", default: "" },
+        { name: "SHADCN_THEME_PRESET", default: "blue" },
+        { name: "SHADCN_THEME_BASE", default: "neutral" },
+        { name: "SHADCN_THEME_RADIUS", default: "default" },
+        { name: "SHADCN_THEME_FONT", default: "inter" },
+        { name: "SHADCN_THEME_PLACEHOLDER", default: "true" },
+      ],
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
